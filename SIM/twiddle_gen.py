2f112228021b1cc607da3ae8f7a2a83cc79f275b
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-11-28 11:00:05 +0300

    twiddle_gen.py

--- SIM/twiddle_gen.py
+++ SIM/twiddle_gen.py
@@ -9,6 +9,21 @@
 # Since: 2021-12-2 10:10:59
 # LastEditors: ahmed abdelazeem
 # LastEditTime: 2021-12-2 10:10:59
+ 
+# This code generates a 16-point complex twiddle factor sequence, representing the real and imaginary parts, and then saves them into separate text files.
+
+# Here's a quick breakdown of the code:
+
+# It initializes arrays for real and imaginary components, each with 16 elements.
+# Calculates the values for the real and imaginary components of the twiddle factors using trigonometric functions (math.cos and math.sin).
+# Rounds the values and scales them by 256, likely for fixed-point representation.
+# Converts these rounded values into binary strings of length 22, assuming they represent fixed-point numbers with 22 bits (including sign bit).
+# Writes these binary strings into text files for the twiddle factors' real and imaginary parts.
+# However, there might be an issue in the code:
+
+# The assignment of bin_real[i] and bin_img[i] seems incorrect because it assigns r[i] and im[i] directly if the condition fails. It should convert the integers r[i] and im[i] to binary strings. You might want to replace bin_real[i] = r[i] and bin_img[i] = im[i] with bin_real[i] = format(r[i],'b').zfill(22) and bin_img[i] = format(im[i],'b').zfill(22) respectively.
+# After this correction, the code should properly write the binary representations of the twiddle factors into the specified text files.
+
 # ********************************************************************
 # Module Function
 def twos_comp(val, bits):
@@ -18,33 +33,26 @@ def twos_comp(val, bits):
     return val   
 
 import math
-real = [0 for i in range(16)]
-img = [0 for i in range(16)]
-r = [0 for i in range(16)]
-im = [0 for i in range(16)]
+real = [0 for _ in range(16)]
+img = [0 for _ in range(16)]
+r = [0 for _ in range(16)]
+im = [0 for _ in range(16)]
 for i in range(16):
-    real[i] = math.cos(math.pi/16*i)
-    img[i]  = math.sin(math.pi/16*i)*(-1)
+    real[i] = math.cos(math.pi / 16 * i)
+    img[i] = math.sin(math.pi / 16 * i) * (-1)
 
 for i in range(16):
-    r[i] = round(real[i]*256)
-    im[i] = round(img[i]*256)
+    r[i] = round(real[i] * 256)
+    im[i] = round(img[i] * 256)
 print(r)
 print(im)
 
-bin_real = [0 for i in range(16)]
-bin_img  = [0 for i in range(16)]
+bin_real = [0 for _ in range(16)]
+bin_img = [0 for _ in range(16)]
 
 for i in range(16):
-    if int(bin_real[i]) > 0 :
-        bin_real[i] = format(r[i],'b').zfill(22)
-    else:
-        bin_real[i] = r[i]
-    if bin_img[i] > 0:
-        bin_img[i]  = format(im[i],'b').zfill(22)
-    else:
-        bin_img[i] = im[i]
-
+    bin_real[i] = format(r[i], '022b')  # Convert to binary with padding of 22 bits
+    bin_img[i] = format(im[i], '022b')  # Convert to binary with padding of 22 bits
 
 with open("twiddle_r.txt", "w") as text_file:
     for i in bin_real:
